01374054ef90fe6e55b93e3f8994ee3223c39de4
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-02-20 16:17:12 +0100

    handle dates in event objects

--- src/components/documentation/content/Event.js
+++ src/components/documentation/content/Event.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 
 export class Event extends Component {
   render () {
+    const event = this.props.event;
+    const format = "DD/MM/YYYY";
     return <div>
-      <div>{this.props.event.name}</div>
-      <div>{this.props.event.description}</div>
+      <div>{event.name}</div>
+      {
+        event.endDate ?
+          <div>Du <Moment date={event.startDate} format={format} /> au <Moment date={event.endDate} format={format}/></div> :
+          <div>Le <Moment date={event.startDate} format={format} /></div>
+      }
+      <div>{event.description}</div>
     </div>;
   }
 }
