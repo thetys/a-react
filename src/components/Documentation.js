28117b1e937d1eaa26d1d5b46e47e7374413836f
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-02-20 10:08:50 +0100

    Create header and documentation components

---
+++ src/components/Documentation.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class Documentation extends Component {
+  render () {
+    return <div className="w100">
+      <div className="w-third">sidebar</div>
+      <div className="w-two-thirds">content</div>
+    </div>;
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render () {
+    return <header className="tc w85">Header</header>;
+  }
+}
