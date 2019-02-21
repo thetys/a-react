83d16f27f3105e14fdaf307d1dfbb122d750b660
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-02-21 13:54:08 +0100

    Init store

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
@@ -18,9 +20,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    < App/>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      < App/>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById('root'),
 );
 
