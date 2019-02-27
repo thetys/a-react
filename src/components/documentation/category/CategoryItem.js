fdb227c38bed6011c8be55901abfc194752ea326
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-02-27 10:02:45 +0100

    Remove unused function

--- src/components/documentation/category/CategoryItem.js
+++ src/components/documentation/category/CategoryItem.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import { ApolloConsumer } from 'react-apollo';
 
 export class CategoryItem extends Component {
-  constructor (props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick () {
-    this.props.onItemSelected(this.props.item.id);
-  }
-
   render () {
     return <ApolloConsumer>
       {client => (
