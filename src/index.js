62cea5b56a4a10c36dec0870dc3071e0d67a0ccc
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-03-01 11:52:11 +0100

    Use apolloClient instead of apolloBoost

--- src/index.js
+++ src/index.js
@@ -4,16 +4,40 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
 import { typeDefs } from './store/typeDefs';
 import { defaults } from './store/defaults';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { withClientState } from 'apollo-link-state';
+import { onError } from 'apollo-link-error';
+
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  clientState: {
-    typeDefs,
-    defaults
-  }
+  cache,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    withClientState({
+      defaults,
+      typeDefs,
+      resolvers: {},
+      cache
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000',
+      credentials: 'same-origin'
+    }),
+  ])
 });
 
 ReactDOM.render(
